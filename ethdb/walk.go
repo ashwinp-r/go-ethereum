a6ef6623c2637bdd032d02ad2ea07a8c4b9e4df4
Alexey Akhunov <akhounov@gmail.com>
2018-09-20 08:24:43 +0100

    Log when rewind

--- ethdb/walk.go
+++ ethdb/walk.go
@@ -182,8 +182,8 @@ func rewindData(db Getter, timestampSrc, timestampDst uint64, df func(bucket, ke
 		var extErr error
 		t.AscendGreaterOrEqual1(min, func(i llrb.Item) bool {
 			item := i.(*PutItem)
-			//preimage, _ := db.Get([]byte("secure-key-"), item.key)
-			//fmt.Printf("bucket: %s, key: %x\n", bucketStr, preimage)
+			preimage, _ := db.Get([]byte("secure-key-"), item.key)
+			fmt.Printf("bucket: %s, key: %x\n", bucketStr, preimage)
 			value, err := db.GetAsOf(bucket[1:], bucket, item.key, timestampDst+1)
 			if err != nil {
 				value = nil
