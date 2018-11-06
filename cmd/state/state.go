8b2ed89b6eff76a8c9099d9cabe7c2a19ecc2dec
Alexey Akhunov <akhounov@gmail.com>
2018-11-06 12:26:56 +0000

    Accounts growth

--- cmd/state/state.go
+++ cmd/state/state.go
@@ -8,19 +8,20 @@ import (
 	//"strings"
 	//"strconv"
 	"flag"
-	"runtime/pprof"
-	"os"
 	"log"
+	"os"
+	"runtime/pprof"
 	//"io/ioutil"
-	//"bufio"
+	"bufio"
+	"sort"
 	"time"
 
 	"github.com/boltdb/bolt"
 	//"github.com/wcharczuk/go-chart"
 	//util "github.com/wcharczuk/go-chart/util"
 
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	//"github.com/ethereum/go-ethereum/consensus/ethash"
 	//"github.com/ethereum/go-ethereum/core"
 	//"github.com/ethereum/go-ethereum/core/rawdb"
@@ -43,9 +44,46 @@ var block = flag.Int("block", 1, "specifies a block number for operation")
 var account = flag.String("account", "0x", "specifies account to investigate")
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
+}
+
+func decodeTimestamp(suffix []byte) (uint64, []byte) {
+	bytecount := int(suffix[0] >> 5)
+	timestamp := uint64(suffix[0] & 0x1f)
+	for i := 1; i < bytecount; i++ {
+		timestamp = (timestamp << 8) | uint64(suffix[i])
+	}
+	return timestamp, suffix[bytecount:]
+}
+
+// Implements sort.Interface
+type TimeSorterInt struct {
+	length     int
+	timestamps []uint64
+	values     []int
+}
+
+func NewTimeSorterInt(length int) TimeSorterInt {
+	return TimeSorterInt{
+		length:     length,
+		timestamps: make([]uint64, length),
+		values:     make([]int, length),
+	}
+}
+
+func (tsi TimeSorterInt) Len() int {
+	return tsi.length
+}
+
+func (tsi TimeSorterInt) Less(i, j int) bool {
+	return tsi.timestamps[i] < tsi.timestamps[j]
+}
+
+func (tsi TimeSorterInt) Swap(i, j int) {
+	tsi.timestamps[i], tsi.timestamps[j] = tsi.timestamps[j], tsi.timestamps[i]
+	tsi.values[i], tsi.values[j] = tsi.values[j], tsi.values[i]
 }
 
 func stateGrowth1() {
@@ -53,37 +91,104 @@ func stateGrowth1() {
 	//db, err := bolt.Open("/home/akhounov/.ethereum/geth/chaindata", 0600, &bolt.Options{ReadOnly: true})
 	//db, err := bolt.Open("/Volumes/tb4/turbo-geth/geth/chaindata", 0600, &bolt.Options{ReadOnly: true})
 	db, err := bolt.Open("/Users/alexeyakhunov/Library/Ethereum/geth/chaindata", 0600, &bolt.Options{ReadOnly: true})
- 	check(err)
- 	defer db.Close()
- 	var count int
- 	err = db.View(func(tx *bolt.Tx) error {
+	check(err)
+	defer db.Close()
+	var count int
+	var maxTimestamp uint64
+	// For each address hash, when was it last accounted
+	lastTimestamps := make(map[common.Hash]uint64)
+	// For each timestamp, how many accounts were created in the state
+	creationsByBlock := make(map[uint64]int)
+	var addrHash common.Hash
+	// Go through the history of account first
+	err = db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(state.AccountsHistoryBucket)
 		if b == nil {
 			return nil
 		}
 		c := b.Cursor()
-		for k, _ := c.First(); k != nil; k, _ = c.Next() {
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			// First 32 bytes is the hash of the address, then timestamp encoding
+			copy(addrHash[:], k[:32])
+			timestamp, _ := decodeTimestamp(k[32:])
+			if timestamp+1 > maxTimestamp {
+				maxTimestamp = timestamp + 1
+			}
+			if len(v) == 0 {
+				creationsByBlock[timestamp]++
+				if lt, ok := lastTimestamps[addrHash]; ok {
+					creationsByBlock[lt]--
+				}
+			}
+			lastTimestamps[addrHash] = timestamp
 			count++
+			if count%100000 == 0 {
+				fmt.Printf("Processed %d records\n", count)
+			}
 		}
 		return nil
- 	})
- 	check(err)
- 	fmt.Printf("Processing took %s\n", time.Since(startTime))
- 	fmt.Printf("Account history records: %d\n", count)
+	})
+	check(err)
+	// Go through the current state
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(state.AccountsBucket)
+		if b == nil {
+			return nil
+		}
+		c := b.Cursor()
+		for k, _ := c.First(); k != nil; k, _ = c.Next() {
+			// First 32 bytes is the hash of the address
+			copy(addrHash[:], k[:32])
+			lastTimestamps[addrHash] = maxTimestamp
+			count++
+			if count%1000 == 0 {
+				fmt.Printf("Processed %d records\n", count)
+			}
+		}
+		return nil
+	})
+	check(err)
+	for _, lt := range lastTimestamps {
+		if lt < maxTimestamp {
+			creationsByBlock[lt]--
+		}
+	}
+	fmt.Printf("Processing took %s\n", time.Since(startTime))
+	fmt.Printf("Account history records: %d\n", count)
+	fmt.Printf("Creating dataset...\n")
+	// Sort accounts by timestamp
+	tsi := NewTimeSorterInt(len(creationsByBlock))
+	idx := 0
+	for timestamp, count := range creationsByBlock {
+		tsi.timestamps[idx] = timestamp
+		tsi.values[idx] = count
+		idx++
+	}
+	sort.Sort(tsi)
+	fmt.Printf("Writing dataset...")
+	f, err := os.Create("accounts_growth.csv")
+	check(err)
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	defer w.Flush()
+	cumulative := 0
+	for i := 0; i < tsi.length; i++ {
+		cumulative += tsi.values[i]
+		fmt.Fprintf(w, "%d, %d\n", tsi.timestamps[i], cumulative)
+	}
 }
 
 func main() {
 	flag.Parse()
-    if *cpuprofile != "" {
-        f, err := os.Create(*cpuprofile)
-        if err != nil {
-            log.Fatal("could not create CPU profile: ", err)
-        }
-        if err := pprof.StartCPUProfile(f); err != nil {
-            log.Fatal("could not start CPU profile: ", err)
-        }
-        defer pprof.StopCPUProfile()
-    }
-    stateGrowth1()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+	stateGrowth1()
 }
-
